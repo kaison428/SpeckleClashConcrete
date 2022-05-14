9364d33a1f821491a9b18ef649d0b6d8e2ff4cb6
Kaison <cheungsiukaison.soniacheung@gmail.com>
2022-05-14 19:05:28 +0800

    major update

--- streamlit_dashboard/main.py
+++ streamlit_dashboard/main.py
@@ -12,7 +12,10 @@ import pandas as pd
 #import plotly express
 import plotly.express as px
 from datetime import date
-#-------------------cd -------
+
+from specklepy.transports.server import ServerTransport
+from specklepy.api import operations
+#---------------------------
 
 #--------------------------
 #PAGE CONFIG
@@ -38,7 +41,7 @@ graphs = st.container()
 #HEADER
 #Page Header
 with header:
-    st.title("Clash Detection Dashboard")
+    st.title("Clash Detection and Defect Dashboard")
 #--------------------------
 
 #--------------------------
@@ -74,6 +77,22 @@ with input:
     cNameFinal = finalCommitCol.selectbox(label="Select your final model", options=commitMessages)
     initialCommit = commitMessages[cNameInitial]
     finalCommit = commitMessages[cNameFinal]
+
+    # # reference base object
+    # objHash = finalCommit.referencedObject
+
+    # # Create the server transport for the specified stream.
+    # transport = ServerTransport(client=client, stream_id=stream.id)
+
+    # # Receive the object
+    # received_base = operations.receive(obj_id=objHash, remote_transport=transport)
+
+    # # The received object, process it as you wish.
+    # print("Received object:", received_base)
+
+    # # TODO: Perform some operation on the received data
+    # member_names = received_base.get_member_names()
+    # print(member_names)
 
     #-------
 #--------------------------
@@ -131,7 +150,7 @@ with viewer:
 #REPORT
 with report:
     # Columns for Cards
-    clashFindingCol, actionPlanCol= st.columns(2)
+    clashFindingCol, actionPlanCol, defectCol= st.columns(3)
 
     height = 400
     with clashFindingCol:
@@ -142,133 +161,7 @@ with report:
         st.subheader("Action Plan")
         actionPlanText = st.text_area('Input your action plan', 'Hi there!', height=height)
 
-    #-------
-    # Columns for Cards
-    branchCol, commitCol, connectorCol, contributorCol = st.columns(4)
-    #-------
+    with defectCol:
+        st.subheader("Defects")
 
-    #-------
-    #Branch Card 💳
-    branchCol.metric(label = "Number of branches", value= stream.branches.totalCount)
-    #branch names as markdown list
-    branchNames = [b.name for b in branches]
-    listToMarkdown(branchNames, branchCol)
-    #-------
-
-    #-------
-    #Commit Card 💳
-    commitCol.metric(label = "Number of commits", value= len(commits))
-    #-------
-
-    #-------
-    #Connector Card 💳
-    #connector list
-    connectorList = [c.sourceApplication for c in commits]
-    #number of connectors
-    connectorCol.metric(label="Number of connectors", value= len(dict.fromkeys(connectorList)))
-    #get connector names
-    connectorNames = list(dict.fromkeys(connectorList))
-    #convert it to markdown list
-    listToMarkdown(connectorNames, connectorCol)
-    #-------
-
-    #-------
-    #Contributor Card 💳
-    contributorCol.metric(label = "Number of contributors", value= len(stream.collaborators))
-    #unique contributor names
-    contributorNames = list(dict.fromkeys([col.name for col in stream.collaborators]))
-    #convert it to markdown list
-    listToMarkdown(contributorNames,contributorCol)
-    #-------
-
-#--------------------------
-
-#--------------------------
-with graphs:
-    st.subheader("Graphs")
-    #COLUMNS FOR CHARTS
-    branch_graph_col, connector_graph_col, collaborator_graph_col = st.columns([2,1,1])
-    
-    #-------
-    #BRANCH GRAPH 📊
-    #branch count dataframe
-    branch_counts = pd.DataFrame([[branch.name, branch.commits.totalCount] for branch in branches])
-    #rename dataframe columns
-    branch_counts.columns = ["branchName", "totalCommits"]
-    #create graph
-    branch_count_graph = px.bar(branch_counts, x=branch_counts.branchName, y=branch_counts.totalCommits, color=branch_counts.branchName, labels={"branchName":"","totalCommits":""})
-    #update layout
-    branch_count_graph.update_layout(
-        showlegend = False,
-        margin = dict(l=1,r=1,t=1,b=1),
-        height=220)
-    #show graph
-    branch_graph_col.plotly_chart(branch_count_graph, use_container_width=True)
-    #-------
-
-    #-------
-    #CONNECTOR CHART 🍩
-    commits= pd.DataFrame.from_dict([c.dict() for c in commits])
-    #get apps from commits
-    apps = commits["sourceApplication"]
-    #reset index
-    apps = apps.value_counts().reset_index()
-    #rename columns
-    apps.columns=["app","count"]
-    #donut chart
-    fig = px.pie(apps, names=apps["app"],values=apps["count"], hole=0.5)
-    #set dimensions of the chart
-    fig.update_layout(
-        showlegend=False,
-        margin=dict(l=1, r=1, t=1, b=1),
-        height=200,
-        yaxis_scaleanchor="x"
-        )
-    #set width of the chart so it uses column width
-    connector_graph_col.plotly_chart(fig, use_container_width=True)
-    #-------
-
-    #-------
-    #COLLABORATOR CHART 🍩
-    #get authors from commits
-    authors = commits["authorName"].value_counts().reset_index()
-    #rename columns
-    authors.columns=["author","count"]
-    #create our chart
-    authorFig = px.pie(authors, names=authors["author"], values=authors["count"],hole=0.5)
-    authorFig.update_layout(
-        showlegend=False,
-        margin=dict(l=1,r=1,t=1,b=1),
-        height=200,
-        yaxis_scaleanchor="x",)
-    collaborator_graph_col.plotly_chart(authorFig, use_container_width=True)   
-    #-------
-    
-    #-------
-    #COMMIT PANDAS TABLE 🔲
-    st.subheader("Commit Activity Timeline 🕒")
-    #created at parameter to dataframe with counts
-    cdate = pd.to_datetime(commits["createdAt"]).dt.date.value_counts().reset_index().sort_values("index")
-    #date range to fill null dates.
-    null_days = pd.date_range(start=cdate["index"].min(), end=cdate["index"].max())
-    #add null days to table
-    cdate = cdate.set_index("index").reindex(null_days, fill_value=0)
-    #reset index
-    cdate = cdate.reset_index()
-    #rename columns
-    cdate.columns = ["date", "count"]
-    #redate indexed dates
-    cdate["date"] = pd.to_datetime(cdate["date"]).dt.date
-    #-------
-
-    #-------
-    #COMMIT ACTIVITY LINE CHART📈
-    #line chart
-    fig = px.line(cdate, x=cdate["date"], y=cdate["count"], markers =True)
-    #recolor line
-    
-    #Show Chart
-    st.plotly_chart(fig, use_container_width=True)
-    #-------
-
-#--------------------------
+        defectType = st.text_area('show defects', 'Hi there!', height=height)
